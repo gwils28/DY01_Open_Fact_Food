21fbb02d953e67c78a470ec7deaa4ed5bcf00812
gwils28 <goma.wilson@hotmail.fr>
2021-05-01 13:41:46 +0200

    add yaml parser

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -2,8 +2,14 @@ import os
 import sys
 import logging as log
 import requests
+import yaml
 
-url = "https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv"
+path = "/home/wils/Data_Project/DY01_Open_Fact_Food/etc/conf/properties.yaml"
+
+def get_url(path):
+    with open(path,"r") as file:
+        ylist = yaml.load(file, Loader=yaml.FullLoader)
+        return ylist["url"]
 
 def fetch_data(url):
     """
@@ -15,11 +21,11 @@ def fetch_data(url):
     with open("/home/wils/Data_Project/DY01_Open_Fact_Food/etc/data/raw_data.csv","wb") as f:
         f.write(r.content)
 
-
 if __name__ =='__main__':
 
     # TODO   if raw data exist  then don't lunch this function.
     # TODO   Add logger
-    # TODO   Add url in a yaml file and read it
 
-    fetch_data(url)
+    # fetch_data(get_url(path))
+
+    print(get_url(path))
